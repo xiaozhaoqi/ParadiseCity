013ffe158a6dc39af46328fd14b8da2c5fffc51c
xiaozhaoqi <zhaoqi.xiao@qq.com>
2024-11-19 22:31:32 +0800

    menu

--- calendar/src/router/index.js
+++ calendar/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Menu from '../views/menu.vue'
 import calendar from '../views/calendar.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
